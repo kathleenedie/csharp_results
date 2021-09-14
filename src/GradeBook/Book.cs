592aebd7575382b6fd6e5229eb5da8bfd9b1de64
Kathleen Edie <kathleen.edie@arnoldclark.com>
2021-09-14 11:28:57 +0100

    first commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        
+        public void AddGrade(double grade)
+        {
+            this.grades.Add(grade);
+        }
+
+        private List<double> grades;
+        private string name;
+
+    }
+}
