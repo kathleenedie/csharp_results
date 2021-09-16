bd5b27ae5ddfa551f04b7a8b09b664984e581f9b
Kathleen Edie <kathleen.edie@arnoldclark.com>
2021-09-16 18:35:00 +0100

    changes before moving to Visual Studio

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -19,11 +19,17 @@ namespace GradeBook
 
             var result = 0.0;
             var highGrade = double.MinValue;
-            foreach(double number in grades) 
+            var lowGrade = double.MaxValue;
+
+            foreach(var number in grades) 
             {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
                 result += number;
             }
             result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
             Console.WriteLine($"The average grade is {result:N1}");
             
         }
